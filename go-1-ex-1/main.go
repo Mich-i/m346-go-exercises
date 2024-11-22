d0e279caa1216049c3c20565b124fc01ac82e5f9
Michael Stutz <michael_stutz@sluz.ch>
2024-11-22 14:40:43 +0100

    Aufgabe 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
+	firstName := "Michael"
+	lastName := "Stutz"
+	dayOfBirth := 24
+	monthOfBirth := 6
+	yearOfBirth := 2007
+	numberOfSiblings := 1
+	heightInMeters := 1.73
+	zodiacSign := 'K'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
