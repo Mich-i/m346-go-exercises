72ffa30bce34f7436a5094b66bdb946541515d00
Michael Stutz <michael_stutz@sluz.ch>
2024-12-06 12:52:55 +0100

    Aufgabe 7

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+	// Definition der Module als Map
+	modules := map[int]string{
+		104: "Programmieren Grundlagen",
+		117: "Systemadministration",
+		346: "Cloud-Lösungen konzipieren und realisieren",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	// Entfernen eines Moduls
+	delete(modules, 117)
+
+	// Hinzufügen eines neuen Moduls
+	modules[210] = "Webentwicklung"
+
+	// Ersetzen eines Moduls
+	modules[346] = "DevOps-Konzepte"
+
 	fmt.Println(modules)
 }
--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,50 @@
 package main
 
+import "fmt"
+
+// Schülerstruktur mit Vor- und Nachnamen
+type Student struct {
+	FirstName string
+	LastName  string
+}
+
+// Klassenstruktur mit einer Liste von Schülern
+type Class struct {
+	Name     string
+	Students []Student
+}
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
-	// TODO: declare a map of modules being attended by multiple classes
-	// TODO: output everything using fmt.Println()
+	// Definition von zwei Klassen mit jeweils drei Schülern
+	class1 := Class{
+		Name: "Klasse 1A",
+		Students: []Student{
+			{"Anna", "Müller"},
+			{"Peter", "Schmidt"},
+			{"Lisa", "Klein"},
+		},
+	}
+
+	class2 := Class{
+		Name: "Klasse 2B",
+		Students: []Student{
+			{"Max", "Mustermann"},
+			{"Sophie", "Meier"},
+			{"Jonas", "Lehmann"},
+		},
+	}
+
+	// Module, die von Klassen besucht werden
+	modules := map[int][]Class{
+		104: {class1, class2},
+		210: {class1},
+		346: {class2},
+	}
+
+	// Ausgabe der Daten
+	fmt.Println("Klassenverwaltung:")
+	fmt.Println(class1)
+	fmt.Println(class2)
+	fmt.Println("Module und besuchte Klassen:")
+	fmt.Println(modules)
 }
