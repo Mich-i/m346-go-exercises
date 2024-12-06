2cec41fe76c3d3a88204b80ba8328d08a9bd4067
Michael Stutz <michael_stutz@sluz.ch>
2024-12-06 13:13:08 +0100

    Aufgabe 10

--- go-3-ex-2/main.go
+++ go-3-ex-2/main.go
@@ -19,16 +19,33 @@ const (
 
 func outputDateRange(zodiacSign rune) {
 	fmt.Printf("%c: ", zodiacSign)
-	// TODO: Replace if, else if branching with switch/case.
-	// TODO: Define all 12 cases...
-	if zodiacSign == Aries {
-		fmt.Println("21.03. - 20.04")
-	} else if zodiacSign == Taurus {
-		fmt.Println("21.04. - 21.05")
-	} else {
-		fmt.Println("")
+
+	switch zodiacSign {
+	case Aries:
+		fmt.Println("21 März bis 20 April")
+	case Taurus:
+		fmt.Println("21 April bis 21 Mai")
+	case Gemini:
+		fmt.Println("22 Mai bis 21 Juni")
+	case Cancer:
+		fmt.Println("22 Juni bis 22 Juli")
+	case Leo:
+		fmt.Println("23 Juli bis 22 August")
+	case Virgo:
+		fmt.Println("23 August bis 22 September")
+	case Libra:
+		fmt.Println("23 September bis 22 Oktober")
+	case Scorpius:
+		fmt.Println("23 Oktober bis 22 November")
+	case Sagittarius:
+		fmt.Println("23 November bis 20 Dezember")
+	case Capricornus:
+		fmt.Println("21 Dezember bis 19 Januar")
+	case Aquarius:
+		fmt.Println("20 Januar bis 18 Februar")
+	case Pisces:
+		fmt.Println("19 Februar bis 20 März")
 	}
-	// TODO: ...and consider a default case.
 }
 
 func main() {
