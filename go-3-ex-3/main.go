f966284768c5fa75dadc363afbd1a59fbc565bd5
Michael Stutz <michael_stutz@sluz.ch>
2024-12-06 13:26:20 +0100

    Aufgabe 11

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -1,10 +1,22 @@
 package main
 
+import "fmt"
+
 const (
 	Lower = 1
 	Upper = 30
 )
 
 func main() {
-	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	for i := 1; i <= 30; i++ {
+		if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%5 == 0 && i%3 == 0 {
+			fmt.Println("FizzBuzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
