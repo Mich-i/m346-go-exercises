cbd3b455f3376a1c54b9cf2fd7a8232a8f9e0329
Michael Stutz <michael_stutz@sluz.ch>
2024-12-13 12:54:58 +0100

    Aufgabe 14

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,25 @@
 package main
 
-// TODO: implement the function computeGrade
+import "fmt"
+
+func computerGrade(gotPoints float64, maxPoints float64) float64 {
+	if maxPoints == 0 {
+		return 1.0
+	}
+
+	note := 1.0 + 5.0*(gotPoints/maxPoints)
+
+	if note > 6.0 {
+		note = 6.0
+	} else if note < 1.0 {
+		note = 1.0
+	}
+
+	return note
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	fmt.Println(computerGrade(17.5, 28.0))
+	fmt.Println(computerGrade(28.0, 28.0))
+	fmt.Println(computerGrade(0.0, 28.0))
 }
--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,16 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
+
+func computeHypotenuse(a float64, b float64) float64 {
+	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+}
 
 func main() {
-	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypotenuse(3.0, 4.0))
+	fmt.Println(computeHypotenuse(7.0, 6.0))
+	fmt.Println(computeHypotenuse(8.0, 15.0))
 }
